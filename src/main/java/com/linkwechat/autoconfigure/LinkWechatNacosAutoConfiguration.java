9dc79b067dc725463178a1480296abe7825a13f6
leejoker <1056650571@qq.com>
2021-06-30 18:22:48 +0800

    fix some bugs

--- src/main/java/com/linkwechat/autoconfigure/LinkWechatNacosAutoConfiguration.java
+++ src/main/java/com/linkwechat/autoconfigure/LinkWechatNacosAutoConfiguration.java
@@ -1,27 +1,54 @@
 package com.linkwechat.autoconfigure;
 
+import com.alibaba.cloud.nacos.NacosConfigAutoConfiguration;
 import com.alibaba.cloud.nacos.NacosDiscoveryProperties;
+import com.alibaba.cloud.nacos.NacosServiceManager;
 import com.alibaba.cloud.nacos.discovery.NacosDiscoveryAutoConfiguration;
+import com.alibaba.cloud.nacos.discovery.NacosServiceDiscovery;
 import com.linkwechat.LinkWechatNacosProperties;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.support.DefaultListableBeanFactory;
 import org.springframework.boot.autoconfigure.AutoConfigureBefore;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 
 /**
  * @author leejoker
  * @version 1.0.0
  */
 @Configuration(proxyBeanMethods = false)
-@AutoConfigureBefore(value = NacosDiscoveryAutoConfiguration.class)
+@AutoConfigureBefore(value = {NacosConfigAutoConfiguration.class, NacosDiscoveryAutoConfiguration.class})
 public class LinkWechatNacosAutoConfiguration {
+    @Autowired
+    private ApplicationContext applicationContext;
+
     @Bean
-    @Primary
-    public NacosDiscoveryProperties linkWechatNacosDiscoveryConfig(LinkWechatNacosProperties linkWechatNacosProperties) {
+    public NacosDiscoveryProperties nacosProperties(LinkWechatNacosProperties linkWechatNacosProperties) {
+        overrideBean(applicationContext);
         NacosDiscoveryProperties nacosDiscoveryProperties = new NacosDiscoveryProperties();
         nacosDiscoveryProperties.setServerAddr(String.format("%s:%d", linkWechatNacosProperties.getHost(), linkWechatNacosProperties.getPort()));
         nacosDiscoveryProperties.setNamespace(linkWechatNacosProperties.getNamespace());
         nacosDiscoveryProperties.setService(linkWechatNacosProperties.getApplicationName());
         return nacosDiscoveryProperties;
+    }
+
+    @Bean
+    public NacosServiceDiscovery nacosServiceDiscovery(
+            NacosDiscoveryProperties nacosProperties,
+            NacosServiceManager nacosServiceManager,
+            LinkWechatNacosProperties linkWechatNacosProperties) {
+        nacosProperties.setService(linkWechatNacosProperties.getApplicationName());
+        return new NacosServiceDiscovery(nacosProperties, nacosServiceManager);
+    }
+
+
+    private void overrideBean(ApplicationContext applicationContext) {
+        ConfigurableApplicationContext context = (ConfigurableApplicationContext) applicationContext;
+        DefaultListableBeanFactory beanFactory = (DefaultListableBeanFactory) context.getBeanFactory();
+        Object configBean = beanFactory.getBean("linkWechatNacosConfig");
+        beanFactory.removeBeanDefinition("linkWechatNacosConfig");
+        beanFactory.registerSingleton("nacosConfigProperties", configBean);
     }
 }
